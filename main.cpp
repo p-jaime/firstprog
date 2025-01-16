b63213cd51f091cdf4e2a0d5f044e99178ed787a
b.ashuraliyev <b.ashuraliyev@newuu.uz>
2025-01-16 10:17:04 +0500

    Hello message and age

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, Wold!" << endl;
+    cout << "How old are you?" << endl;
+    cout << "Age: " << endl;
+    cin>>age;
+    cout << "Your age is :" << age << endl;
+    return 0;
+}
